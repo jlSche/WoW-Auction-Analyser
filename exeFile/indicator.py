a9b48bdd786182cd9da9b854eced7ac544d50f23
jlsche <jlinsche@gmail.com>
2015-06-26 21:56:31 +0800

    modify indicator.py

--- exeFile/indicator.py
+++ exeFile/indicator.py
@@ -11,21 +11,15 @@ def gatherItems():
 # return mean item appearance of each week
 def getItemWeeklySupply(item_id=None, realm=None, fraction=None):
 	# find the item first
-	df = pd.read_csv('../workingDir/0313-1012/' + realm + '_' + fraction + '.csv')
+	df = pd.read_csv('../corr_result/auction_detail/' + realm + '_' + fraction)
 	df = df[df['Item ID'] == item_id]
 
-	# remove rows that AH Quantity is zero
-	df = df[df['AH Quantity']!=0]
+	return df
 
-	# add "Week Num" column
-	df['Week Num'] = df['PMktPrice Date'].apply(DataHandling.toWeekNumber)
-
-	df = df.groupby(['Week Num'], as_index=False)
-	return df['AH Quantity'].mean()
-
-
-
-#def calCorrelation():
+def calCorrelation(item_id=None, realm=None, fraction=None, merged_df=None):
+	corr, pval = stats.pearsonr(merged_df['AH Quantity'], merged_df['Profit'])
+	new_record = pd.DataFrame([{'Realm': realm, 'Fraction': fraction, 'Item ID': item_id, 'Corr': corr, 'p Val': pval}])
+	return new_record
 
 
 def main():
@@ -37,22 +31,18 @@ def main():
 		item_id = int(row['Item ID'])
 		realm = row['Realm']
 		fraction = row['Fraction']
-		print 'now processing', item_id, realm, fraction
+		print 'now processing', item_id, realm, fraction, '\t',
 
 		weekly_supply = getItemWeeklySupply(item_id, realm, fraction)
-		#print weekly_supply
 		weekly_profit = profit_df[(profit_df['Realm']==realm) & (profit_df['Fraction']==fraction)]
-		#print weekly_profit
-
 		merged_df = weekly_supply.merge(weekly_profit, on='Week Num')
-		print merged_df
-		#'''
-		corr, pval = stats.pearsonr(merged_df['AH Quantity'], merged_df['Profit'])
-		new_record = pd.DataFrame([{'Realm': realm, 'Fraction': fraction, 'Item ID': item_id, 'Corr': corr, 'p Val': pval}])
+		#print merged_df
+
+		new_record = calCorrelation(item_id, realm, fraction, merged_df)
 		corr_result_df = corr_result_df.append(new_record, ignore_index=True)
-		#'''
-		break
-	print corr_result_df
+		print new_record
+
+	corr_result_df.to_csv('../corr_result/indicator_corr.csv', index=False)
 
 if __name__ == '__main__':
 	main()
